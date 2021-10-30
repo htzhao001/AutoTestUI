4018ab34f7eadeb102c6a7d820c803bcbb352b94
htzhao <921043431@qq.com>
2021-10-30 12:33:56 +0800

    basepage增加日志记录功能

--- src/common/basepage.py
+++ src/common/basepage.py
@@ -2,6 +2,7 @@
 # 创建时间： 2021/10/20 21:41
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+from src.common.getlog import MyLog
 
 
 CLICK = 'click'
@@ -19,6 +20,7 @@ class BasePage:
 
     def __init__(self, driver):
         self.driver = driver
+        self.log = MyLog()
 
     def _get_selector(self, selector_text):
         """
@@ -26,6 +28,7 @@ class BasePage:
         :param selector_text:
         :return:
         """
+        method = ''
         if '=>' not in selector_text:
             method = By.ID
             selector = selector_text
@@ -34,6 +37,8 @@ class BasePage:
             selector = selector_text.split('=>')[1]
             if method_text == 'xpath' or method_text == 'x':
                 method = By.XPATH
+            elif method_text == 'id' or method_text == 'i':
+                method = By.ID
             elif method_text == 'css' or method_text == 'c':
                 method = By.CSS_SELECTOR
             elif method_text == 'text' or method_text == 't':
@@ -45,8 +50,8 @@ class BasePage:
             elif method_text == 'tagname' or method_text == 'tag':
                 method = By.TAG_NAME
             else:
-                print('定位信息有误！')
-
+                self.log.error(f'定位信息有误！请检查 {selector_text}')
+        self.log.debug(f"已完成定位方式解析,定位方式：{method}; 定位信息：'{selector}'")
         return method, selector
 
     def _find(self, selector_text):
@@ -56,8 +61,13 @@ class BasePage:
         :return:
         """
         method, selector = self._get_selector(selector_text)
-        ele = self.driver.find_element(method, selector)
-        return ele
+        try:
+            ele = self.driver.find_element(method, selector)
+            self.log.info(f"已找到元素 '{ele.text}'。")
+            return ele
+        except Exception as msg:
+            self.log.warning(f'查找元素失败！请检查页面是否正常加载或等待时间是否合适。信息：{msg}')
+
 
     def _finds(self, selector_text):
         """
@@ -66,8 +76,12 @@ class BasePage:
         :return:
         """
         method, selector = self._get_selector(selector_text)
-        ele_list = self.driver.find_elements(method, selector)
-        return ele_list
+        try:
+            ele_list = self.driver.find_elements(method, selector)
+            self.log.info(f"已找到元素组。")
+            return ele_list
+        except Exception as msg:
+            self.log.warning(f'查找元素失败！请检查页面是否正常加载或等待时间是否合适。信息：{msg}')
 
     def get(self, url):
         """
@@ -76,57 +90,85 @@ class BasePage:
         :return:
         """
         self.driver.get(url)
+        self.log.info(f'已成功访问url：{url}')
 
     def get_title(self):
         """
         获取当前页面标题
         :return:
         """
+        self.log.debug(f"以获取当前页面标题：{self.driver.title} 。")
         return self.driver.title
 
     # 页面前进
     def forward(self):
+        self.log.debug(f"浏览器页面跳转后一页。")
         self.driver.forward()
 
     # 页面后退
     def back(self):
+        self.log.debug(f"浏览器页面跳转前一页。")
         self.driver.back()
 
     # 跳转frame
     def to_frame(self, frame):
-        self.driver.switch_to.frame(frame)
+        try:
+            self.driver.switch_to.frame(frame)
+            self.log.info(f"浏览器页面跳至内嵌frame页面。")
+        except Exception as msg:
+            self.log.error(f"跳转内嵌frame失败，信息：{msg}")
 
     # 跳回上一层frame
     def to_parent_frame(self):
         self.driver.switch_to.parent_frame()
+        self.log.info(f"浏览器页面跳至上一层frame页面。")
 
     # 跳回最外层frame
     def out_frame(self):
         self.driver.switch_to.default_content()
+        self.log.info(f"浏览器页面跳至最外层frame页面。")
 
     # 跳转警告窗口
     def to_alert(self):
-        self.driver.switch_to.alert()
+        try:
+            self.driver.switch_to.alert()
+            self.log.info(f"浏览器页面跳至警告弹窗。")
+        except Exception as msg:
+            self.log.error(f"跳转警告弹窗失败，信息：{msg}")
+
 
     # 接受警告窗口
     def alert_accept(self):
         self.driver.switch_to.alert.accept()
+        self.log.warning(f"警告弹窗已接受。")
 
     # 取消警告窗口
     def alert_dismiss(self):
         self.driver.switch_to.alert.dismiss()
+        self.log.warning(f"警告弹窗已取消。")
 
     # 执行js语句
     def _js(self, js_):
-        self.driver.execute_script(js_)
+        try:
+            self.driver.execute_script(js_)
+            self.log.info(f"已成功执行js语句：{js_}")
+        except Exception as msg:
+            self.log.error(f"执行语句:'{js_}'失败！报错信息为：{msg}")
 
     # 页面截图
     def get_screenshots(self, file_path):
         self.driver.get_screenshot_as_file(file_path)
+        self.log.info(f"已成功截图当前页面！")
 
     # 右键动作
     def _context_click(self, selector):
-        ActionChains(self.driver).context_click(self._find(selector)).perform()
+        ele = self._find(selector)
+        try:
+            ActionChains(self.driver).context_click(ele).perform()
+            self.log.info(f"已成功右键点击元素：{ele.text}。")
+        except Exception as msg:
+            self.log.error(f"右键点击元素 '{ele.text}' 失败！报错信息为：{msg}")
+
 
     # 拖动动作
     def _drop_to_point(self, selector, point):
@@ -134,21 +176,39 @@ class BasePage:
 
     # 双击动作
     def _double_click(self, selector):
-        ActionChains(self.driver).double_click(self._find(selector)).perform()
+        ele = self._find(selector)
+        try:
+            ActionChains(self.driver).double_click(ele).perform()
+            self.log.info(f"已成功双击元素：{ele.text}。")
+        except Exception as msg:
+            self.log.error(f"双击元素 '{ele.text}' 失败！报错信息为：{msg}")
 
     # 移动至动作
     def _move_to(self, selector):
-        ActionChains(self.driver).move_to_element(self._find(selector)).perform()
+        ele = self._find(selector)
+        try:
+            ActionChains(self.driver).move_to_element(ele).perform()
+            self.log.info(f"鼠标已成功移动至元素：{ele.text} 处。")
+        except Exception as msg:
+            self.log.error(f"鼠标移动到 '{ele.text}' 失败！报错信息为：{msg}")
 
     # 统一动作--点击
     def _click(self, selector):
         ele = self._find(selector)
-        ele.click()
+        try:
+            ele.click()
+            self.log.info(f"已成功点击元素：{ele.text} 。")
+        except Exception as msg:
+            self.log.error(f"点击元素： '{ele.text}' 失败！报错信息为：{msg}")
 
     # 统一动作--输入值
     def _send_keys(self, selector, value):
         ele = self._find(selector)
-        ele.send_keys(value)
+        try:
+            ele.send_keys(value)
+            self.log.info(f"已成功在输入框中输入：{value} 。")
+        except Exception as msg:
+            self.log.error(f"输入内容： '{value}' 失败！报错信息为：{msg}")
 
     # 统一动作--js点击
     def _js_click(self, selector):
@@ -158,7 +218,8 @@ class BasePage:
         elif method == By.CSS_SELECTOR:
             js_ = f"document.querySelector('{selector_text}').click();"
         else:
-            print('定位方式有误！只能使用xpath或者css')
+            js_ = f"console.log('{method}: {selector_text} 无效')"
+            self.log.warning(f'定位方式有误！只能使用xpath或者css。当前定位方式为: {method}')
         self._js(js_)
 
     # 统一动作--js输入值
@@ -175,7 +236,8 @@ class BasePage:
         elif method == By.CSS_SELECTOR:
             js_ = f"document.querySelector('{selector_text}').innerText() = '{value}';"
         else:
-            print('定位方式有误！只能使用xpath或者css')
+            js_ = f"console.log('{method}: {selector_text}  输入:{value}失败！')"
+            self.log.warning(f'定位方式有误！只能使用xpath或者css。当前定位方式为: {method}')
         self._js(js_)
 
     # 统一动作装饰器函数
@@ -184,6 +246,7 @@ class BasePage:
     """
     def action(func):
         def deal(self, action_=None, value=None):
+            # 若动作词没有输入，则返回定位字符串
             if action_ is None:
                 return func(self)
 
@@ -206,6 +269,6 @@ class BasePage:
             elif action_ == 'finds':
                 self._finds(func(self))
             else:
-                print('动作暂不支持！期待新版吧。')
+                self.log.warning(f'{action_}动作词无效！请检查动作词或向动作库中加入更多的交互动作吧！')
 
         return deal
