85a27c89ef70c3a9c730ac2daf4fcbe5f7657cf3
htzhao <921043431@qq.com>
2021-10-30 14:11:36 +0800

    流程初始化增加了流程名称，修改原有初始化参数

--- src/cases/test_baidu_home_flow.py
+++ src/cases/test_baidu_home_flow.py
@@ -3,6 +3,7 @@
 from src.common.mytest import MyTest
 from src.common.baseflow import Para
 from src.flows.prepare_flow import PrepareFlow
+from src.flows.aftermath_flow import AfterMathFlow
 from src.flows.baidu_home_flow import SearchFlow
 
 
@@ -14,14 +15,15 @@ class TestBaiduFlows(MyTest):
 
     @classmethod
     def setUpClass(cls):
-        cls.prepare_flow = PrepareFlow(cls.driver)
+        cls.prepare_flow = PrepareFlow(name='环境准备流程', driver=cls.driver)
         cls.prepare_flow.execute()
 
     @classmethod
     def tearDownClass(cls):
-        cls.driver.quit()
+        cls.aftermath_flow = AfterMathFlow(name='浏览器退出流程', driver=cls.driver)
+        cls.aftermath_flow.execute()
 
     def test_search_flow(self):
-        search_flow = SearchFlow(self.driver, self.para)
+        search_flow = SearchFlow(name='搜索流程', driver=self.driver, para=self.para)
         search_flow.with_start_page(self.prepare_flow.end_page).execute()
         self.assertEqual_('test', 'test', msg='测试消息', driver=self.driver)
